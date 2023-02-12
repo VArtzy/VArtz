bdabb2953a6a78c5eb580d978cac39e725d68b77
VArtzy <farrelnikoson@gmail.com>
2023-02-12 23:58:01 +0700

    12

---
+++ app/terms-condition/head.tsx
@@ -0,0 +1,18 @@
+export default function Head() {
+    return (
+        <>
+            <title>
+                VArtz - Syarat & Ketentuan dan berbagai macam pertanyaan.
+            </title>
+            <meta
+                content="width=device-width, initial-scale=1"
+                name="viewport"
+            />
+            <meta
+                name="description"
+                content="Syarat & Ketentuan dan berbagai pertanyaan umum yang bisa kami jawab untuk meyakinkanmu"
+            />
+            <link rel="icon" href="/favicon.ico" />
+        </>
+    )
+}
---
+++ app/terms-condition/page.tsx
@@ -0,0 +1,216 @@
+import Foo from "../../components/Foo"
+const About = () => {
+    return (
+        <>
+            <div className="max-w-3xl mx-auto px-8 grid place-content-center gap-8 tracking-wider leading-relaxed">
+                <h1 className="text-3xl md:text-4xl w-full font-bold mt-24">
+                    Terms & Condition
+                </h1>
+                <h2 className="text-2xl md:text-3xl w-full font-bold lg:mb-4">
+                    Kondisi Syarat & Ketentuan. Berikut ini adalah syarat &
+                    ketentuan yang dibuat demi kebaikan bersama.
+                </h2>
+                <div className="p-4">
+                    <p className="mb-4">
+                        Kami akan mulai mengerjakan website ketika customer
+                        sudah melakukan pembayaran uang muka (DP) sebesar 50%.
+                        Sisanya dibayarkan setelah website selesai dikerjakan.
+                    </p>
+
+                    <p className="mb-4">
+                        Lakukan pemesanan di platform whatsapp, pembayaran
+                        secara tunai atau down payment (DP) minimal 50% (Hanya
+                        transfer tunai ke rekening BCA atas nama Eko Purnomo)
+                    </p>
+
+                    <p className="mb-4">
+                        Website akan dibuat sesuai dengan fitur template yang
+                        disajikan. Jika ada tambahan fitur, maka akan dikenakan
+                        biaya tambahan.
+                    </p>
+
+                    <ul className="list-disc pl-4">
+                        <li className="mb-2">
+                            Materi website tidak boleh mengandung unsur
+                            pornografi, perjudian, money game atau unsur yang
+                            melanggar hukum lainnya.
+                        </li>
+                        <li className="mb-2">
+                            Logo yang sudah termasuk dalam harga pembuatan
+                            adalah logo standar. (Jika perlukan)
+                        </li>
+                        <li className="mb-2">
+                            Selesai dalam 1-5 hari kerja dengan maksimal
+                            pengerjaan seminggu kecuali website custom dengan
+                            masa perbaikan selama 7 hari.
+                        </li>
+                        <li className="mb-2">
+                            Harga sudah termasuk domain dan hosting untuk 1
+                            tahun.
+                        </li>
+                        <li className="mb-2">
+                            Tahun berikutnya cukup membayar Rp 499.000 (50%)
+                            untuk perpanjangan domain dan hosting paket BASIC
+                            tahunan. Rp 99.000 (50%) untuk paket BASIC bulanan.
+                            Rp 399.000 (80%) untuk paket SUPER. Rp 149.000 (50%)
+                            untuk paket CUSTOM bulanan. Rp 499.000 (50%) untuk
+                            pake CUSTOM bulanan. Harga bisa berubah
+                            sewaktu-waktu tergantung harga dari penyedia jasa
+                            hosting.
+                        </li>
+                        <li className="mb-2">
+                            Jika anda sudah mempunyai domain dan hosting, maka
+                            anda cukup mengurangi harga pembuatan website
+                            sebesar Rp.150.000
+                        </li>
+                        <li className="mb-2">
+                            Layanan tidak mencakup pembuatan script yang
+                            bersifat pemrograman.
+                        </li>
+                        <li className="mb-2">
+                            Syarat dan ketentuan ini dibuat demi kebaikan
+                            bersama. Dengan memesan jasa kami, berarti anda
+                            setuju pada syarat dan ketentuan di atas.
+                        </li>
+                    </ul>
+                </div>
+                <a
+                    href="https://wa.me/6289628196420?text=Hai,%20min.%20Saya%20berminat%20untuk%20membuat%20website.%20Berikut%20ini%20adalah%20data%20saya%3A%0A%0ANama%3A%0ABidang%20Usaha%3A%0A%0A%28Mohon%20isi%20data%20di%20atas%20lalu%20kirim%29"
+                    className="block text-md rounded bg-springGreen px-4 py-2"
+                >
+                    Order Sekarang
+                </a>
+
+                <h2 className="text-3xl md:text-4xl w-full font-bold mt-24">
+                    FAQs
+                </h2>
+                <h2 className="text-2xl md:text-3xl w-full font-bold lg:mb-4">
+                    Frequently Asked Questions. Pertanyaan yang paling banyak
+                    ditanyakan dan mungkin menjawab pertanyaan kamu.
+                </h2>
+
+                <div className="p-4">
+                    <div className="accordion">
+                        <div className="accordion-item">
+                            <div className="accordion-header">
+                                <h3 className="font-medium">
+                                    Apa itu VArtz jasa pembuatan web? 👋
+                                </h3>
+                                <span className="accordion-icon"></span>
+                            </div>
+                            <div className="accordion-body">
+                                merupakan personil jasa yang menyediakan
+                                berbagai macam layanan IT mulai dari Pembuatan
+                                Website seperti toko online, company profile,
+                                portofiolio, personal dan lainnya, Pengembangan
+                                Aplikasi lengkap disertai pemeliharaan atau
+                                maintenance berkala.
+                            </div>
+                        </div>
+                        <div className="accordion-item">
+                            <div className="accordion-header">
+                                <h3 className="font-medium">
+                                    Jasa pembuatan termasuk pembuatan apa saja?
+                                </h3>
+                                <span className="accordion-icon"></span>
+                            </div>
+                            <div className="accordion-body">
+                                Secara umum layanan berlangganan sudah termasuk
+                                biaya pembuatan, nama domain, hosting/server,
+                                optimasi mobile, optimasi SEO (agar website Anda
+                                masuk ke mesin pencarian), pengoperasian dan
+                                pemeliharaan.
+                            </div>
+                        </div>
+                        <div className="accordion-item">
+                            <div className="accordion-header">
+                                <h3 className="font-medium">
+                                    Data apa saja yang perlu saya sediakan? 🤟
+                                </h3>
+                                <span className="accordion-icon"></span>
+                            </div>
+                            <div className="accordion-body">
+                                Kami membutuhkan beberapa data Anda sebagai
+                                bahan untuk membangun website terkait informasi
+                                yang ingin Anda tampilkan pada situs Anda.
+                                Antara lain: referensi (jika ada), data
+                                pengelola ( nama, alamat, nomor kontak ) company
+                                profile / profil bisnis, logo, deskripsi produk,
+                                foto / gambar produk, dan artikel yang
+                                diperlukan.
+                            </div>
+                        </div>
+                        <div className="accordion-item">
+                            <div className="accordion-header">
+                                <h3 className="font-medium">
+                                    Apakah ada garansi?
+                                </h3>
+                                <span className="accordion-icon"></span>
+                            </div>
+                            <div className="accordion-body">
+                                Kami tumbuh ketika usaha Anda sukses. Garansi
+                                sudah kami sertakan dalam setiap paket pembuatan
+                                web ini, dan kami siap membantu untuk melakukan
+                                perbaikan gratis ketika terjadi website “error”.
+                            </div>
+                        </div>
+                        <div className="accordion-item">
+                            <div className="accordion-header">
+                                <h3 className="font-medium">
+                                    Bagaimana saya dapat mengembangkan bisnis
+                                    saya?
+                                </h3>
+                                <span className="accordion-icon"></span>
+                            </div>
+                            <div className="accordion-body">
+                                Kami tumbuh bersama usaha Anda. Anda dapat
+                                membaca blog milik kami yang selalu update dan
+                                relevan dengan perkembagan zaman. Tentu anda
+                                akan mendapatkan ilmu yang banyak dari sana dan
+                                selamat membaca! (Jika anda menggunakan jasa
+                                VArtz, secara otomatis anda akan terdaftar di
+                                blog 😀)
+                            </div>
+                        </div>
+                        <div className="accordion-item">
+                            <div className="accordion-header">
+                                <h3 className="font-medium">
+                                    Bisakah saya menggunakan template sendiri?
+                                </h3>
+                                <span className="accordion-icon"></span>
+                            </div>
+                            <div className="accordion-body">
+                                Jika Anda sudah mempunyai template sendiri,
+                                silakan kirimkan kepada kami. Dengan senang hati
+                                kami akan mengeditkan dan menguploadnya untuk
+                                Anda.
+                            </div>
+                        </div>
+                        <div className="accordion-item">
+                            <div className="accordion-header">
+                                <h3 className="font-medium">
+                                    Maintenance apa saja yang termasuk dalam
+                                    paket di atas? 🤖
+                                </h3>
+                                <span className="accordion-icon"></span>
+                            </div>
+                            <div className="accordion-body">
+                                Maintenance yang kami lakukan yaitu memastikan
+                                website tidak down dan tidak kena hack.
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <a
+                    href="https://wa.me/6289628196420?text=Hai,%20min.%20Saya%20berminat%20untuk%20membuat%20website.%20Berikut%20ini%20adalah%20data%20saya%3A%0A%0ANama%3A%0ABidang%20Usaha%3A%0A%0A%28Mohon%20isi%20data%20di%20atas%20lalu%20kirim%29"
+                    className="block text-md rounded bg-springGreen px-4 py-2"
+                >
+                    Order Sekarang
+                </a>
+            </div>
+            <Foo />
+        </>
+    )
+}
+
+export default About
