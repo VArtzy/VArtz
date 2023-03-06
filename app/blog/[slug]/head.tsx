0a72063f9f3b6af72a610e78b1dab20cef230ae6
VArtzy <farrelnikoson@gmail.com>
2023-03-06 18:59:20 +0700

    fix og image blogpost

--- app/blog/[slug]/head.tsx
+++ app/blog/[slug]/head.tsx
@@ -27,8 +27,13 @@ export default function Head({ params }: { params: { slug: string } }) {
                 content={`https://vartz.web.id/blog/${slug}`}
             />
             <meta property="og:locale" content="id_ID" />
-            <meta property="og:image" content="https://vartz.web.id/og/logo.png" />
+            <meta
+                property="og:image"
+                content="https://vartz.web.id/og/logo.png"
+            />
             <meta property="og:image:type" content="image/png" />
+            <meta property="og:image:width" content="256" />
+            <meta property="og:image:height" content="256" />
             <meta property="og:image:alt" content="VArtz Blog" />
             <meta property="og:description" content={post.data.subtitle} />
             <meta property="og:type" content="article" />
