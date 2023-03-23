90667d43db2a3e92495e53cb30a75dc90f25e93f
VArtzy <farrelnikoson@gmail.com>
2023-03-23 21:15:26 +0700

    asd

---
+++ app/quiz/head.tsx
@@ -0,0 +1,18 @@
+export default function Head() {
+    return (
+        <>
+            <title>
+                VArtz - Kuis mudah mendapatkan website impian terbaik.
+            </title>
+            <meta
+                content="width=device-width, initial-scale=1"
+                name="viewport"
+            />
+            <meta
+                name="description"
+                content="Luangkan sedikit waktu untuk mengisi diagnosis untuk mendapatkan website impian dengan performa maksimal."
+            />
+            <link rel="icon" href="/favicon.ico" />
+        </>
+    )
+}
