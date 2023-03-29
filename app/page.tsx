8f952175cb4bdc301b4abafc0ff55c6258ad6ba4
VArtzy <farrelnikoson@gmail.com>
2023-03-29 14:05:06 +0700

    t

--- app/page.tsx
+++ app/page.tsx
@@ -144,56 +144,131 @@ const Home = () => {
                 </div>
             )}
 <LocalBusinessJsonLd
-  type="WebSite"
-  id="https://vartz.web.id"
-  name="VArtz"
-  description="Jasa Pembuatan Situs Web Berkualitas dan Terjangkau untuk Bisnis Kecil Menengah."
-  url="https://vartz.web.id"
-  telephone="+629611905599"
+  type="Store"
+  id="http://davesdeptstore.example.com"
+  name="Dave's Department Store"
+  description="Dave's latest department store in San Jose, now open"
+  url="http://www.example.com/store-locator/sl/San-Jose-Westgate-Store/1427"
+  telephone="+14088717984"
   address={{
-                    streetAddress: "105 Gatot Subroto",
-                    addressLocality: "Laweyan",
-                    addressRegion: "Surakarta",
-                    postalCode: "57111",
-                    addressCountry: "ID",
-                }}
+    streetAddress: '1600 Saratoga Ave',
+    addressLocality: 'San Jose',
+    addressRegion: 'CA',
+    postalCode: '95129',
+    addressCountry: 'US',
+  }}
+  geo={{
+    latitude: '37.293058',
+    longitude: '-121.988331',
+  }}
+  images={[
+    'https://example.com/photos/1x1/photo.jpg',
+    'https://example.com/photos/4x3/photo.jpg',
+    'https://example.com/photos/16x9/photo.jpg',
+  ]}
+  sameAs={[
+    'www.company-website-url1.dev',
+    'www.company-website-url2.dev',
+    'www.company-website-url3.dev',
+  ]}
+  openingHours={[
+    {
+      opens: '08:00',
+      closes: '23:59',
+      dayOfWeek: [
+        'Monday',
+        'Tuesday',
+        'Wednesday',
+        'Thursday',
+        'Friday',
+        'Saturday',
+      ],
+      validFrom: '2019-12-23',
+      validThrough: '2020-04-02',
+    },
+    {
+      opens: '14:00',
+      closes: '20:00',
+      dayOfWeek: 'Sunday',
+      validFrom: '2019-12-23',
+      validThrough: '2020-04-02',
+    },
+  ]}
+  rating={{
+    ratingValue: '4.5',
+    ratingCount: '2',
+  }}
+  review={[
+    {
+      author: 'John Doe',
+      datePublished: '2006-05-04',
+      name: 'A masterpiece of literature',
+      reviewBody:
+        'I really enjoyed this book. It captures the essential challenge people face as they try make sense of their lives and grow to adulthood.',
+      reviewRating: {
+        bestRating: '5',
+        worstRating: '1',
+        reviewAspect: 'Ambiance',
+        ratingValue: '4',
+      },
+    },
+    {
+      author: 'Bob Smith',
+      datePublished: '2006-06-15',
+      name: 'A good read.',
+      reviewBody: "Catcher in the Rye is a fun book. It's a good book to read.",
+      reviewRating: {
+        ratingValue: '4',
+      },
+    },
+  ]}
   makesOffer={[
     {
       priceSpecification: {
         type: 'UnitPriceSpecification',
-        priceCurrency: 'ID',
-        price: 'Rp 599.000',
+        priceCurrency: 'EUR',
+        price: '1000-10000',
       },
       itemOffered: {
-        name: 'Jasa Pembuatan Web Paket Basic',
+        name: 'Motion Design Services',
         description:
-          'Paket pembuatan situs web dengan fitur dasar pilihan',
+          'We are the expert of animation and motion design productions.',
       },
     },
     {
       priceSpecification: {
         type: 'UnitPriceSpecification',
-        priceCurrency: 'IDR',
-        price: 'Rp 999.000',
+        priceCurrency: 'EUR',
+        price: '2000-10000',
       },
       itemOffered: {
-        name: 'Jasa Pembuatan Web Paket Custom',
+        name: 'Branding Services',
         description:
-          'Paket pembuatan situs web dengan fitur lebih lengkap dan kustom',
-    }},
-{
-      priceSpecification: {
-        type: 'UnitPriceSpecification',
-        priceCurrency: 'IDR',
-        price: 'Rp 1.799.000',
-      },
-      itemOffered: {
-        name: 'Jasa Pembuatan Web Paket Super',
-        description:
-          'Paket pembuatan situs web dengan fitur lengkap dan desain super',
+          'Real footage is a powerful tool when it comes to show what the business is about. Can be used to present your company, show your factory, promote a product packshot, or just tell any story. It can help create emotional links with your audience by showing punchy images.',
       },
     },
   ]}
+  areaServed={[
+    {
+      geoMidpoint: {
+        latitude: '41.108237',
+        longitude: '-80.642982',
+      },
+      geoRadius: '1000',
+    },
+    {
+      geoMidpoint: {
+        latitude: '51.108237',
+        longitude: '-80.642982',
+      },
+      geoRadius: '1000',
+    },
+  ]}
+  action={{
+    actionName: 'potentialAction',
+    actionType: 'ReviewAction',
+    target: 'https://www.example.com/review/this/business',
+  }}
 />
         </div>
     )
