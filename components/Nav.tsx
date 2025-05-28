b5342594432c3ac1de691b824df6d10da864bb2b
VArch <farrelnikoson@gmail.com>
2025-05-28 23:44:04 +0700

    fix cntn

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,7 +1,7 @@
 "use client"
 import Image from "next/image"
 import Link from "next/link"
-import { useRouter } from 'next/router'
+import { useRouter } from 'next/navigation'
 import React, { useState } from "react"
 import { BsArrowRight } from "react-icons/bs"
 import useReadingProgress from "./Progress"
